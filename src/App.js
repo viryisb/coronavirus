e4630b0b574453fb191f5443bfa93cb7a4151abc
Virginia Bernasconi <bernasconivir@gmail.com>
2020-03-30 00:19:37 -0300

    agrega tabla

--- src/App.js
+++ src/App.js
@@ -6,19 +6,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> mapa del coronavirus
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+      <table class="table table-sm">
+  <thead>
+    <tr>
+     
+      <th scope="col">Name</th>
+      <th scope="col">Age</th>
+      <th scope="col">Gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Mark</td>
+      <td>65</td>
+      <td>f</td>
+    </tr>
+    <tr>
+      
+      <td>Rodolfo</td>
+      <td>87</td>
+      <td>m</td>
+    </tr>
+    <tr>
+    <td>Ciliberto</td>
+      <td>30</td>
+      <td>m</td>
+    </tr>
+  </tbody>
+</table>
+
+      </body>
     </div>
   );
 }
