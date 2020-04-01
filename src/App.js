81238d5f6af3caa850a7fd4002cff9da0fddcc04
Virginia Bernasconi <bernasconivir@gmail.com>
2020-03-31 22:29:34 -0300

    agrega botón para descargar csv

--- src/App.js
+++ src/App.js
@@ -15,28 +15,49 @@ class App extends React.Component{
     this.state.infectedPeople.forEach((infected)=>{
       let name= infected.first_name;
       let lastName=infected.last_name;
-      let age=infected.age; 
+      let age=(infected.age>365)? parseInt(infected.age /365) : infected.age + " days"
       
        infectedTable.push(<tr className={(infected.live)? "live":"dead"}>
         <td>{name}  {lastName}</td>
-      <td>{age}</td>
-      <td>{(infected.female)? <span>F</span>:<span>M</span>}</td>
+      <td>{age }</td>
+      <td>{ (infected.female)? <span>F</span>:<span>M</span>}</td>
       </tr>)
     }
   );
   return infectedTable; 
 }
+downloadInfected(){
+  let csv= "Name, Age, Gender, Live\n"
+this.state.infectedPeople.forEach((infected)=>{
+let name= infected.first_name;
+      let lastName=infected.last_name;
+      let gender= infected.female? "male" : "female"
+      let age=(infected.age>365)? parseInt(infected.age /365) : infected.age + " days";
+      let live=!! infected.live;
+      csv += name+ '' + lastName + ',' + age + ',' + gender + ',' + live +';';
+    })
+
+//hago el download
+const element = document.createElement("a");
+const file = new Blob([csv], {type: "text/csv"});
+element.href = URL.createObjectURL(file);
+element.download = "Infected.csv";
+document.body.appendChild(element);
+element.click();
+element.parentNode.removeChild(element);
+}
 
 
 render(){
-  return (
-    
-    <div className="App">
-      <header className="App-header">
-        <p> mapa del coronavirus
-        </p>
-      </header>
-      <table class="table table-sm">
+return (
+
+<div className="App">
+  <header className="App-header">
+    <p> mapa del coronavirus
+    </p>
+  </header>
+<button onClick={() => {this.downloadInfected()}}>descargar</button>
+  <table class="table table-sm">
   <thead>
     <tr>
      
